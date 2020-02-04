4724e1aa173f37360a19f92981e5ef43251990ae
dieezs <diegomagasouza@gmail.com>
2020-02-04 16:06:15 -0300

    aulas e exercicios

---
+++ cursoemvideo/myExamplesExercises/diferencaDeIgual.js
@@ -0,0 +1,18 @@
+var n = 2;
+
+// == COMPARA A DIFERENCA ENTRE O VALOR CONTIDO ENTRE A STRING 
+//    E O NUMERO. O RESULTADO SERA TRUE POIS SERA COMPARADO O VALOR DO MESMO.
+if (n == '2') {
+    console.log('true')
+} else {
+    console.log('false')
+}
+
+// === COMPARA O TIPO DA VARIAVEL E O VALOR A SER COMPARADO. 
+//     NESTE CASO SERA FALSE POIS STRING EH DIFERENTE DE NUMBER
+if (n === '2') {
+    console.log('true')
+} else {
+    console.log('false')
+}
+
