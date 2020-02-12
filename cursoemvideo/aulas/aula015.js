7ceb1a809db1c7a159154b34f26eafd09cc94241
dieezs <diegomagasouza@gmail.com>
2020-02-12 15:34:17 -0300

    for in e for of examples

---
+++ cursoemvideo/aulas/aula015.js
@@ -0,0 +1,33 @@
+var num = [98, 39, 1, 38, 22, 6];
+
+const casa ={
+    area: 100,
+    largura: 5,
+    andares: 2
+}
+
+
+
+// for(var i = 0; i<=num.length; i++){
+//     console.log(num[i]);
+// }
+
+// for(var pos in num){
+//     console.log(num[pos])
+// }
+
+// for(var pos of num){
+//     console.log(pos)
+// }
+
+for(let comodo in casa){
+    console.log(comodo)
+    console.log(casa[comodo])
+
+}
+
+// FOR
+
+// IN = percorre as propriedades de um objeto enumerado. Lista os artibutos de um objeto
+
+// OF = percore os valores de um objeto e mostra os mesmos.
