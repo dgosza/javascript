d7332206257c3c2efa31a6ecebb8d4b09c357d19
Diego Souza <diegomagasouza@gmail.com>
2020-02-16 22:59:18 -0300

    operacoes com arrays

--- rocketseat/javascript_es6/main.js
+++ rocketseat/javascript_es6/main.js
@@ -1,31 +1,9 @@
-class List{
-    constructor(){
-        this.data = [] 
-    }
-
-    add(data){
-        this.data.push(data) 
-        console.log("dentro do add " + data)
-    }
-}
-
-
-class TodoList extends List{
-    constructor(){
-        super();
-        this.usuario = "Diego";
-    }
-
-    mostraUsuario(){
-        console.log(this.usuario)
+class Matematica{
+    
+    static soma(a, b){
+        return a + b;
     }
 
 }
 
-var MinhaLista = new TodoList();
-
-
-document.getElementById('novoTodo').onclick = function(){
-    MinhaLista.add('oi');
-    MinhaLista.mostraUsuario();
-}
+console.log(Matematica.soma(1, 2)) 
