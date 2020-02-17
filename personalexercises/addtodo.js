8897701746a0ec161dc9d0301e12097d2c1c9073
dieezs <diegomagasouza@gmail.com>
2020-02-17 15:11:42 -0300

    personal exercise on javascript - simple to do list

---
+++ personalexercises/addtodo.js
@@ -0,0 +1,50 @@
+var inputDo = document.getElementById('inputDo')
+var inputDoing = document.getElementById('inputDoing')
+var inputDone = document.getElementById('inputDone')
+
+var buttonDo = document.getElementById('btnDo')
+var buttonDoing = document.getElementById('btnDoing')
+var buttonDone = document.getElementById('btnDone')
+
+
+function criaElemento(inputId, iconParameter, textParamer) {
+    var input = inputId
+    var icon = document.createElement('i');
+    icon.setAttribute('class', 'material-icons');
+    icon.style.color = 'grey';
+    icon.innerText = iconParameter
+
+    var text = document.createElement('p');
+    text.innerText = textParamer;
+    text.style.marginTop = '-1px';
+
+    var divLeft = document.createElement('div');
+    divLeft.setAttribute('class', 'col s12 l2');
+    divLeft.appendChild(icon)
+
+    var divRight = document.createElement('div');
+    divRight.setAttribute('class', 'col s12 l10');
+    divRight.appendChild(text);
+
+    input.appendChild(divLeft);
+    input.appendChild(divRight);
+
+    switch (inputId.id) {
+        case 'listDo':
+            var bloco = 'Do'
+            break
+        case 'listDoing':
+            var bloco = 'Doing'
+            break
+        case 'listDone':
+            var bloco = 'Done'
+            break
+    }
+
+    var msgAdicionado = `<span>Item Adicionado no bloco ${bloco}!</span>`;
+    M.toast({ html: msgAdicionado });
+}
+
+buttonDo.onclick = function () { criaElemento(document.getElementById('listDo'), 'details', inputDo.value); inputDo.value = '';  };
+buttonDoing.onclick = function () { criaElemento(document.getElementById('listDoing'), 'chat_bubble_outline', inputDoing.value); inputDoing.value = ''; };
+buttonDone.onclick = function () { criaElemento(document.getElementById('listDone'), 'check', inputDone.value), inputDone.value = '';};
